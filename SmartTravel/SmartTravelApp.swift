667d6c1df0797ea43f194ce6e074292cda06782d
LionelCC <LionelCC@users.noreply.github.com>
2023-12-23 17:50:30 -0500

    Initial Commit

---
+++ SmartTravel/SmartTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTravelApp.swift
+//  SmartTravel
+//
+//  Created by Lionel Chen on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
